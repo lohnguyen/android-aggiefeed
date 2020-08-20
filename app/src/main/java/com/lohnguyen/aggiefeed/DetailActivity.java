e7d71cd266155ae56d7281ea9cee1354afd616e1
Long H. Nguyen <lohnguyen47@gmail.com>
2020-08-20 11:45:19 -0700

    Fix activity switch with finish()

--- app/src/main/java/com/lohnguyen/aggiefeed/DetailActivity.java
+++ app/src/main/java/com/lohnguyen/aggiefeed/DetailActivity.java
@@ -26,7 +26,6 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     public void goToMain(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        this.finish();
     }
 }
