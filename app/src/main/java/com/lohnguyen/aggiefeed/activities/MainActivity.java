90217fc12fd9d3bb90c79c7b43058a29a0401615
Long H. Nguyen <lohnguyen47@gmail.com>
2020-09-03 15:33:34 -0700

    Create search with RxJava

--- app/src/main/java/com/lohnguyen/aggiefeed/activities/MainActivity.java
+++ app/src/main/java/com/lohnguyen/aggiefeed/activities/MainActivity.java
@@ -1,19 +1,26 @@
 package com.lohnguyen.aggiefeed.activities;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Observer;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.MenuItemCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 
 import com.lohnguyen.aggiefeed.entities.FeedItem;
 import com.lohnguyen.aggiefeed.R;
@@ -21,6 +28,16 @@ import com.lohnguyen.aggiefeed.adapters.FeedItemAdapter;
 import com.lohnguyen.aggiefeed.viewmodels.MainViewModel;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity implements FeedItemAdapter.OnFeedItemListener, SwipeRefreshLayout.OnRefreshListener {
 
@@ -39,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements FeedItemAdapter.O
     private RecyclerView recyclerView;
     private FeedItemAdapter adapter = new FeedItemAdapter(allFeedItems, MainActivity.this);
 
+    private CompositeDisposable disposables = new CompositeDisposable();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,23 +73,19 @@ public class MainActivity extends AppCompatActivity implements FeedItemAdapter.O
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        mainViewModel.getAll().observe(this, new Observer<List<FeedItem>>() {
-            @Override
-            public void onChanged(@Nullable final List<FeedItem> feedItems) {
-                allFeedItems = feedItems;
-                adapter.setFeedItems(allFeedItems);
-                recyclerView.setAdapter(adapter);
-            }
+        mainViewModel.getAll().observe(this, feedItems -> {
+            allFeedItems = feedItems;
+            adapter.setFeedItems(allFeedItems);
+            recyclerView.setAdapter(adapter);
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        setUpSearchItem(menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -105,5 +120,84 @@ public class MainActivity extends AppCompatActivity implements FeedItemAdapter.O
         intent.putExtra(EXTRA_END_DATE, feedItem.getEndDate());
 
         startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
+
+    private void setUpSearchItem(Menu menu) {
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        MenuItem searchMenuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) searchMenuItem.getActionView();
+
+        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchAutoComplete.setHintTextColor(ContextCompat.getColor(this.getApplicationContext(), R.color.accent));
+        searchAutoComplete.setTextColor(ContextCompat.getColor(this.getApplicationContext(), R.color.accent));
+
+        ImageView searchCloseIcon = (ImageView)searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+        searchCloseIcon.setImageResource(R.drawable.ic_clear_24);
+
+        ImageView searchIcon = (ImageView)searchView.findViewById(androidx.appcompat.R.id.search_button);
+        searchIcon.setImageResource(R.drawable.ic_search_24);
+
+//        ImageView searchMagIcon = (ImageView)searchView.findViewById(androidx.appcompat.R.id.search_mag_icon);
+//        searchMagIcon.setImageResource(R.drawable.ic_search_24);
+
+        RxSearchObservable.fromView(searchView)
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .filter(text -> !text.isEmpty() && text.length() >= 3)
+                .map(text -> text.toLowerCase().trim())
+                .distinctUntilChanged()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        disposables.add(d);
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.e("howdy", s);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    public static class RxSearchObservable {
+
+        public static Observable<String> fromView(SearchView searchView) {
+            final PublishSubject<String> subject = PublishSubject.create();
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    subject.onComplete();
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String text) {
+                    subject.onNext(text);
+                    return true;
+                }
+            });
+
+            return subject;
+        }
     }
 }
